a8a896be0c965a3382d724b08540b2398fee0e4b
Union-luxarous-lts <chandrashekhar5566p@gmail.com>
2025-03-22 16:23:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ async def upload(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**[📽️] Vid_ID:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{#_MR}.mkv\n**PHASE** » **{raw_text0}**'
+                cc = f'**Vid_ID🥦:** {str(count).zfill(3)}.** {𝗻𝗮𝗺𝗲𝟭}{MR}.mkv\n**PHASE** » **{raw_text0}**'
                 cc1 = f'**[📁] Pdf_ID:** {str(count).zfill(3)}. {𝗻𝗮𝗺𝗲𝟭}{MR}.pdf \n**𝔹ᴀᴛᴄʜ** » **{raw_text0}**'
                 if "drive" in url:
                     try:
